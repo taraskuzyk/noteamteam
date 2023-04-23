adbaa6f05fbec50630682c9d171362f4413b3c33
Shahnoor Chowdhury Eshan <eshan.world@gmail.com>
2023-04-23 14:45:12 -0600

    changes

--- src/views/CleanRequestPage.vue
+++ src/views/CleanRequestPage.vue
@@ -2,21 +2,48 @@
 	<ion-page>
 		<ion-header>
 			<ion-toolbar>
-				<ion-title>Profile</ion-title>
+				<ion-title>Request</ion-title>
 			</ion-toolbar>
 		</ion-header>
 		<ion-content :fullscreen="true">
 			<ion-header collapse="condense">
 				<ion-toolbar>
-					<ion-title size="large">Profile</ion-title>
+					<ion-title size="large">Request</ion-title>
 				</ion-toolbar>
 			</ion-header>
 
-			<ExploreContainer name="Profile page"/>
+			<ion-list>
+				<ion-item>
+					<ion-label>Address</ion-label>
+					<ion-textarea placeholder="Type the address which needs to be cleaned"></ion-textarea>
+				</ion-item>
+				<ion-item>
+					<ion-label>Reward point</ion-label>
+					<ion-select placeholder="Select point">
+						<ion-select-option value="1">1 Point</ion-select-option>
+						<ion-select-option value="2">2 Points</ion-select-option>
+						<ion-select-option value="3">3 Points</ion-select-option>
+						<ion-select-option value="4">4 Points</ion-select-option>
+						<ion-select-option value="5">5 Points</ion-select-option>
+					</ion-select>
+				</ion-item>
+			</ion-list>
 		</ion-content>
 	</ion-page>
 </template>
 
 <script setup lang="ts">
-import {IonContent, IonHeader, IonPage, IonTitle, IonToolbar} from "@ionic/vue";
+import {
+	IonContent,
+	IonHeader,
+	IonPage,
+	IonTitle,
+	IonToolbar,
+	IonItem,
+	IonSelect,
+	IonSelectOption,
+	IonList,
+	IonTextarea,
+	IonLabel
+} from "@ionic/vue";
 import ExploreContainer from "@/components/ExploreContainer.vue";</script>
